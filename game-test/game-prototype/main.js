ba39a4fe55bb236759732b807b711fb206ce4275
Malcolm J Sackler <mal@Malcolm-Sacklers-Computer.local>
2016-03-03 09:40:35 -0500

    more moves

--- game-test/game-prototype/main.js
+++ game-test/game-prototype/main.js
@@ -15,21 +15,53 @@ combatLog = document.querySelector('.combatLog')
 
 var player = {
   Hp: 100,
-  Atk: 15,
-  Def: 13,
+  Atk: 20,
+  Def: 12,
+  Spell: 20,
   Speed: 11,
+  Mana: 70,
 }
 var enemy = {
   Hp: 120,
   Atk: 18,
-  Def: 10,
+  Def: 15,
+  Spell: 10,
   Speed: 9,
+  Mana: 50,
 }
-var at = function(){
-  enemy.Hp = enemy.Hp - 10;
-  enemyHp.innerText = "HP " + enemy.Hp;
+
+var moveChoice = function(){
+  Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+}
+var enemyAtk = function(){
+  player.Hp = player.Hp - (enemy.Atk - player.Def);
+  playerHp.innerText = "Hp " + player.Hp;
+  combatLog.innerText = "Enemy Attacks for " + (enemy.Atk - player.Def) + " damage!"
+}
+var playerAtk = function(){
+  enemy.Hp = enemy.Hp - (player.Atk - enemy.Def);
+  enemyHp.innerText = "Hp " + enemy.Hp;
+  combatLog.innerText = "You Attack for " + (player.Atk - enemy.Def) + " damage!"
+}
+var enemyDefUp = function(){
+  enemy.Def = enemy.Def + 3;
 
 }
+
+var enemyFireball = function(){
+  player.Hp = player.Hp - enemy.Spell;
+  playerHp.innerText = "Hp " + player.Hp;
+  combatLog.innerText = "Enemy cast Fireball! " + enemy.Spell + " damage!"
+
+
+
+}
+// damages bad guy and updates hp
+// var test = function(){
+//   enemy.Hp = enemy.Hp - 10;
+//   enemyHp.innerText = "HP " + enemy.Hp;
+//
+// }
 
 
 
